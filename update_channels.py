9edc57126cf747a7e13019d2325bf0f4482d4876
Myeongeum Yoo <gmyungum@gmail.com>
2022-09-11 20:17:03 +0900

    통계 분리

--- update_channels.py
+++ update_channels.py
@@ -1,20 +1,9 @@
 import requests
 from pymongo import MongoClient
-from collections import Counter
-from konlpy.tag import *
 import time
-from tqdm import tqdm
-from datetime import datetime, timedelta
-import re
+from datetime import datetime
 
 UPDATE_PERIOD = 120
-GRAPH_NODE_MAX = 30
-GRAPH_FETCH_WORD_SIZE = 10000
-GRAPH_WORD_RANGE = 100
-WORD_STATISTICS_MIN_COUNT = 5
-SUDDEN_INCREASE_RANK_SIZE = 30000
-mecab = Mecab()
-trash_list = open('불용어.txt', 'r', encoding='utf8').read().splitlines()
 
 # connection info
 with open('setting.txt', 'r') as f:
@@ -26,7 +15,6 @@ client = None
 db = None
 
 
-# 1.
 def update_live_channels():
     with requests.session() as s:
         # get access token
@@ -54,94 +42,6 @@ def update_live_channels():
         else:
             print(res.text)
 
-
-# 2.
-def update_word_statistics(today):
-    
-    # get difference set
-    not_prepared = list(set(db['chats'].distinct('date')) - set(db['word_statistics'].distinct('date')))
-    # convert str into date
-    not_prepared = [datetime.strptime(date_str, '%Y-%m-%d').date() for date_str in not_prepared]
-    # remove not finished date
-    not_prepared = [date for date in not_prepared if date < today]
-
-    if len(not_prepared) > 0:
-        for date in tqdm(not_prepared):
-            date_str = date.strftime('%Y-%m-%d')
-            print('make word statistics:', date_str)
-            dic = dict()
-            for doc_chat in tqdm(list(db['chats'].find({'date': date_str}, {'_id': 0, 'channel': 1, 'text': 1}))):
-                if doc_chat['channel'] not in dic:
-                    dic[doc_chat['channel']] = Counter()
-                for token in mecab.nouns(doc_chat['text']):
-                    dic[doc_chat['channel']][token] += 1
-            
-            docs = []
-            for channel, counter in dic.items():
-                words = dict()
-                for word, count in counter.most_common():
-                    if count < WORD_STATISTICS_MIN_COUNT:
-                        break
-                    words[word] = count
-                docs.append({
-                    'date' : date_str,
-                    'channel' : channel,
-                    'words' : words
-                })
-            db['word_statistics'].insert_many(docs)          
-
-
-#3.
-def update_word_sudden_increase(today):
-    def get_docs(date):
-        return list(db['chats'].find({'date': date}, {'_id': 0, 'text': 1}))
-
-
-    def get_word_list(date, n):
-        docs = get_docs(date)
-        counter = Counter()
-        for doc in tqdm(docs):
-            without_hangul = re.compile('[^ 가-힣+]')
-            hangul = without_hangul.sub('', doc['text'])
-            for st in range(len(hangul)):
-                # 미등록 형태소는 첫번째 형태소로 나올 확률이 높다. 참조 : 김보겸 이재성(2016), 「확률 기반 미등록 단어 분리 및 태깅」
-                if hangul[st] != ' ' and (st == 0 or hangul[st - 1] == ' '):
-                    for ed in range(st + 2, len(hangul) + 1):
-                        counter[hangul[st:ed]] += 1
-        return counter.most_common(n)
-
-    # get difference set
-    not_prepared = list(set(db['chats'].distinct('date')) - set(db['word_increase'].distinct('date')))
-    # convert str into date
-    not_prepared = [datetime.strptime(date_str, '%Y-%m-%d').date() for date_str in not_prepared]
-    # remove not finished date
-    not_prepared = [date for date in not_prepared if date < today]
-
-    if len(not_prepared) > 0:
-        for date in tqdm(not_prepared):
-            date_str = date.strftime('%Y-%m-%d')
-            print('make word increase:', date_str)
-            pre_date = date - timedelta(days=1)
-            pre_date_str = pre_date.strftime('%Y-%m-%d')
-            pre_dic = dict(get_word_list(pre_date_str, SUDDEN_INCREASE_RANK_SIZE))
-
-            # calculate rate of change
-            increase = []
-            for word, count in get_word_list(date_str, SUDDEN_INCREASE_RANK_SIZE):
-                if word in pre_dic:
-                    pre_count = pre_dic[word]
-                    increase.append((word, 100.0 * (count - pre_count) / pre_count))
-
-            # remove sub string
-            increase_dic = dict(increase)
-            for word, count in sorted(increase, reverse=True, key=lambda x: len(x[0])):
-                increase_dic.pop(word[1:], None)
-                increase_dic.pop(word[:-1], None)
-            
-            # insert
-            db['word_increase'].insert_one({'date': date_str, 'data': increase_dic})
-
-
 while True:
     try:
         # db connection
@@ -150,8 +50,6 @@ while True:
         start_time =  datetime.now()
         
         update_live_channels()
-        update_word_statistics(start_time.date())
-        update_word_sudden_increase(start_time.date())
 
     except Exception as e:
         print('Exception :', str(e))
